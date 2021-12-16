07faccb1019524a4da208ea6204fbed59236866a
nicholas-rose <nickrose@uchicago.edu>
2021-12-16 10:38:35 -0600

    added some logging

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/exrisk'));
 
 app.get('/*', (req,res,next) => {
+    console.log(req.params);
     res.sendFile(path.join(__dirname + '/dist/exrisk/index.html'));
 });
 
 app.listen(process.env.PORT || 8000);
+console.log(`app running on port ${process.env.PORT || 8000}`);
