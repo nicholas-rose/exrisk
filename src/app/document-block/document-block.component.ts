f576b691d59e2b155cc0b024bd5d229413482b36
nicholas-rose <nickrose@uchicago.edu>
2021-12-20 15:32:32 -0600

    updated links for resume page to gdocs

--- src/app/document-block/document-block.component.ts
+++ src/app/document-block/document-block.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-document-block',
   template: `
-  <a href='/assets/{{this.docname}}' target='_blank' download>
+  <a href='{{this.link}}' target='_blank' download>
     <div class='div-hover'>
       <div class='container' style='outline:1px solid grey;padding:10px;'>
         <div class='row justify-content-center align-items-center'>
             <div class="col-sm-auto">
-                <img src='/assets/{{this.docname}}' height='100px' width='auto'>
+                <img src='/assets/{{this.preview}}' height='100px' width='auto'>
             </div>
             <div class="col-sm">
                 <p>{{this.description}}</p>
@@ -20,6 +20,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./document-block.component.css']
 })
 export class DocumentBlockComponent {
-  @Input() docname: string = 'nickrose-resume-web.png';
+  @Input() preview: string = 'nickrose-resume-web.png';
+  @Input() link: string = 'https://www.exrisk.net/resume';
   @Input() description: string = "couldn't find the document. come back later!";
 }
